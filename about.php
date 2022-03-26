bad91bf48cd44b3d70ed32f68ba8671d493ed98f
fybsc1737 <55774608+fybsc1737@users.noreply.github.com>
2022-03-26 13:22:03 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+    <!--font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+    <!-- custom css file link -->
+     <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<!-- header section starts -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travelicious.</a>
+
+   <nav class="navbar">
+       <a href="home.php">home</a>
+       <a href="about.php">about</a>
+       <a href="package.php">package</a>
+       <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section end -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>“Travel is the main thing you purchase that makes you more extravagant”. We, at Organization Name, swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly.
+        We have been moving excellent encounters for a considerable length of time through our cutting-edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters. </p>
+        <p>Through our exceptionally curated occasion bundles, we need to take you on an adventure where you personally enjoy the stunning magnificence of America and far-off terrains. We need you to observe sensational scenes that are a long way past your creative ability.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destinations</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide service</span>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!-- about section end -->
+
+<!-- reviews section starts -->
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper wrapper">
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>The whole experience was wonderful, the booking agent viator failed in a few aspects to ensure their 
+                    clients the cruise ship where up to date with the relevant sales and clients booked.</p>
+                <h3>john deo</h3>
+                <span>traveler</span>
+                <img src="images/pic-1.png" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>The airport transfers we booked were great! The driver was waiting for us in the airport arrival hall with a name board and the drive to town went smooth.
+                I would recommend to anyone the services.</p>
+                <h3>Beatriz </h3>
+                <span>traveler</span>
+                <img src="images/pic-2.png" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Cannot fault any part of this day tour. Very informative en well thought through.
+                 Loved every part of the day out.</p>
+                <h3>Brayden</h3>
+                <span>traveler</span>
+                <img src="images/pic-3.png" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Friendly and helpful staff. Kept their promise to revert and followed up to book the ticket</p>
+                <h3>Amara</h3>
+                <span>traveler</span>
+                <img src="images/pic-4.png" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>The tour was delightful, full of interesting stories and perspectives.
+                We all had a great time and would contact Nehal again.
+                Thank you.</p>
+                <h3>manish</h3>
+                <span>traveler</span>
+                <img src="images/pic-5.png" alt="">
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Easy booking process</p>
+                <h3>Li na</h3>
+                <span>traveler</span>
+                <img src="images/pic-6.png" alt="">
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+           <h3>quick links</h3>
+           <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+           <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+           <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+           <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+           <h3>extra links</h3>
+           <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+           <h3>contact info</h3>
+           <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+           <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+           <a href="#"> <i class="fas fa-envelope"></i> nehalt@888gmail.com </a>
+           <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400091 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkdin </a>  
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>mr. Nehal Tawade</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link -->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ book.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+    <!--font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+    <!-- custom css file link -->
+     <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<!-- header section starts -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travelicious.</a>
+
+   <nav class="navbar">
+       <a href="home.php">home</a>
+       <a href="about.php">about</a>
+       <a href="package.php">package</a>
+       <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section end -->
+
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+    <h1>book now</h1>
+</div>
+
+<!-- booking section starts -->
+
+<section class="booking">
+
+    <h1 class="heading-title">book your trip!</h1>
+
+    <form action="book_form.php" method="post" class="book-form">
+
+        <div class="flex">
+            <div class="inputbox">
+                <span>name :</span>
+                <input type="text" placeholder="enter your name" name="name">
+            </div>
+            <div class="inputbox">
+                <span>email :</span>
+                <input type="email" placeholder="enter your email" name="email">
+            </div>
+            <div class="inputbox">
+                <span>phone :</span>
+                <input type="number" placeholder="enter your number" name="phone">
+            </div>
+            <div class="inputbox">
+                <span>address :</span>
+                <input type="text" placeholder="enter your address" name="address">
+            </div>
+            <div class="inputbox">
+                <span>where to :</span>
+                <input type="text" placeholder="place you want to visit" name="location">
+            </div>
+            <div class="inputbox">
+                <span>how many :</span>
+                <input type="number" placeholder="number of guests" name="guests">
+            </div>
+            <div class="inputbox">
+                <span>arrivals :</span>
+                <input type="date" name="arrivals">
+            </div>
+            <div class="inputbox">
+                <span>leaving :</span>
+                <input type="date" name="leaving">
+            </div>
+        </div>
+
+        <input type="submit" value="submit" class="btn" name="send">
+
+    </form>
+
+</section>
+
+<!-- booking section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+           <h3>quick links</h3>
+           <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+           <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+           <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+           <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+           <h3>extra links</h3>
+           <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+           <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+           <h3>contact info</h3>
+           <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+           <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+           <a href="#"> <i class="fas fa-envelope"></i> nehalt@888gmail.com </a>
+           <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400091 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkdin </a>  
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>mr. Nehal Tawade</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link -->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
